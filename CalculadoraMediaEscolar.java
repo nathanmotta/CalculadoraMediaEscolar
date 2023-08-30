faebb0cd352e3d8a37e67cf64da9e86736546a29
nathanmotta <142407439+nathanmotta@users.noreply.github.com>
2023-08-30 12:27:31 -0300

    Add files via upload

---
+++ CalculadoraMediaEscolar.java
@@ -0,0 +1,28 @@
+package application;
+
+import java.util.Scanner;
+
+public class CalculadoraMediaEscolar {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite a nota da prova: ");
+        double notaProva = scanner.nextDouble();
+
+        System.out.print("Digite a nota do trabalho: ");
+        double notaTrabalho = scanner.nextDouble();
+
+        double media = (notaProva + notaTrabalho) / 2;
+
+        System.out.println("A média do aluno é: " + media);
+
+        // Verificar se o aluno foi aprovado ou reprovado
+        if (media >= 7.0) {
+            System.out.println("Parabéns, você é um gênio!");
+        } else {
+            System.out.println("Você é burro, estude mais.");
+        }
+
+        scanner.close();
+    }
+}
